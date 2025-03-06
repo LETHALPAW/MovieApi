9be123e432c1df7359700224f4613e63f9335b6c
Yigit Alp Balli <yigitaballi@gmail.co>
2025-03-06 11:40:24 +0300

    Yeni projeler eklendi ve yapılandırmalar güncellendi
    MovieApi.sln dosyasına "MovieApi.Domain", "MovieApi.Persistence", "MovieApi.WebApi" ve "MovieApi.Application" projeleri eklendi.
    MovieApi.Application.csproj, MovieApi.Domain.csproj ve MovieApi.Persistence.csproj dosyalarına .NET 9.0 hedef çerçevesi eklendi.
    Yeni komut ve sorgu sınıfları oluşturuldu.
    Varlık sınıfları (Cast, Category, Movie, Review, Tag) eklendi.
    DbContext yapılandırması ve varlık setleri tanımlandı.
    Veritabanı migrasyonları eklendi.
    Günlükleme ayarları yapıldı.
    Program.cs dosyasına uygulama yapılandırması ve OpenAPI desteği eklendi.
    launchSettings.json dosyasına başlatma ayarları eklendi.

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext :DbContext
+    {
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=GE-ABALLI15\\MSSQLSERVER2022;database=MovieApiDb;user id=sa;password=Btnsft1958;TrustServerCertificate=True;");
+		}
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Movie> Movies { get; set; }
+		public DbSet<Review> Reviews { get; set; }
+		public DbSet<Tag> Tags { get; set; }
+		public DbSet<Cast> Casts { get; set; }
+	
+	}
+}
