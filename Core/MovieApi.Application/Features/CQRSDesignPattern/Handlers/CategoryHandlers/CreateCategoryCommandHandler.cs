158ef47a2349345fbf245390a61e108cced20458
Yigit Alp Balli <yigitaballi@gmail.co>
2025-03-06 11:58:58 +0300

    Origin RepProje referansları eklendi ve yeni sınıf oluşturuldu
    `MovieApi.Application.csproj` dosyasına `MovieApi.Persistence` ve `MovieApi.Domain` projelerine referanslar eklendi. `Folder` öğeleri kaldırıldı.
    
    `CreateCategoryCommandHandler.cs` dosyasında, `MovieContext` sınıfını kullanmak için gerekli `using` ifadeleri eklendi ve yeni bir `CreateCategoryCommandHandler` sınıfı tanımlanarak yapıcı metot oluşturuldu.

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/CategoryHandlers/CreateCategoryCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers
+{
+    public class CreateCategoryCommandHandler
+    {
+        private readonly MovieContext _context;
+
+		public CreateCategoryCommandHandler(MovieContext context)
+		{
+			_context = context;
+		}
+		public async Task<int> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+		{
+			var category = new Category
+			{
+				CategoryName = request.CategoryName
+			};
+			_context.Categories.Add(category);
+			await _context.SaveChangesAsync();
+			return category.CategoryId;
+		}
+	}
+}
